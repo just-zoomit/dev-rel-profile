b129f7e44d8ccf0bd50189dfe1b77f952ee14f23
Donte Small <donte.small@zoom.us>
2025-08-18 00:42:54 -0400

    Added codelab links

--- src/lib/content/codelabs/codelabs.ts
+++ src/lib/content/codelabs/codelabs.ts
@@ -34,5 +34,21 @@ export const codelabs: Codelab[] = [
     technologies: ["Express", "MongoDB", "Node.js"],
     url: "https://just-zoomit.github.io/enable-rtms-zoomapp-sdk/#0",
   },
+  {
+    title: "Get started with Zoom Contact Center apps SDK",
+    description: "Create a Zoom Contact Center app.",
+    duration: "30 min",
+    difficulty: "Intermediate",
+    technologies: [ "Next.JS", "TailwindCSS","ShadCN",],
+    url: "https://just-zoomit.github.io/zcc-get-started-codelab/#0",
+  },
+  {
+    title: "Getting started with Zoom Apps SDK codelab",
+    description: "Create a Zoom app.",
+    duration: "30 min",
+    difficulty: "Intermediate",
+    technologies: [ "Next.JS", "TailwindCSS","ShadCN",],
+    url: "https://just-zoomit.github.io/get-started-with-zoomapps-sdk/#0",
+  },
   // …add more here
 ];
